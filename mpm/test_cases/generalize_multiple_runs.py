41f682cbc0ee6a7eddfe785b5388d057d5ec243e
Roderik Ploszek <roderik.ploszek@gmail.com>
2023-05-14 23:52:46 +0200

    fix: use iterated tree

--- mpm/test_cases/generalize_multiple_runs.py
+++ mpm/test_cases/generalize_multiple_runs.py
@@ -4,7 +4,7 @@ from mpm.test_cases.helpers import TestCaseContext
 
 
 def test_core(ctx: TestCaseContext) -> None:
-    trees = [NpmTree(tree=ctx.tree, deep=True) for tree in ctx.trees]
+    trees = [NpmTree(tree=tree, deep=True) for tree in ctx.trees]
     regex_tree = generalize_mupltiple_runs(ctx.db, *trees)
     tree = merge_tree(*trees, regex_tree)
     tree.move_generalized_to_regexp()
