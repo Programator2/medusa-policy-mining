8b23c86cfcc5a7bfd9650741aa98ba3923be867f
Roderik Ploszek <roderik.ploszek@gmail.com>
2023-05-29 00:05:18 +0200

    chore: remove unneeded method call

--- mpm/test_cases/generalize_multiple_runs.py
+++ mpm/test_cases/generalize_multiple_runs.py
@@ -7,5 +7,4 @@ def test_core(ctx: TestCaseContext) -> None:
     trees = [NpmTree(tree=tree, deep=True) for tree in ctx.trees]
     regex_tree = generalize_mupltiple_runs(ctx.db, *trees)
     tree = merge_tree(*trees, regex_tree)
-    tree.move_generalized_to_regexp()
     ctx.tree = tree
